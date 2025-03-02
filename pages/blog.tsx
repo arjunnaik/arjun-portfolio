3cef827c0a4c0dd000eb790813952d73ab22f976
Arjun Naik <55954515+arjunnaik@users.noreply.github.com>
2025-03-02 10:40:35 +0530

    Create blog.tsx

---
+++ pages/blog.tsx
@@ -0,0 +1,10 @@
+const Blog: React.FC = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>🚀 Exciting updates are on the way! Stay tuned—I`ll be updating this page soon with more details about me and my work. 🎯✨</p>
+    </div>
+  );
+};
+
+export default Blog;
