c40c7cc1a1923db924715a397edc10bf61daff9a
Arjun Naik <55954515+arjunnaik@users.noreply.github.com>
2025-03-02 10:39:10 +0530

    Create projects.tsx

---
+++ pages/projects.tsx
@@ -0,0 +1,12 @@
+const Projects: React.FC = () => {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>
+        🚀 Exciting updates are on the way! Stay tuned—I`ll be updating this
+        page soon with more details about me and my work. 🎯✨
+      </p>
+    </div>
+  );
+};
+export default Projects;
